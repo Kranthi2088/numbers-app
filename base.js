115f393b4d86c1003b92a9db6ddb3ce4a6be5c6b
Dunna, Kranthi Kumar <kdunna@iu.edu>
2024-01-25 21:20:57 -0500

    final_commit

--- base.js
+++ base.js
@@ -1,6 +1,10 @@
-function add(){
+async function add(){
     let number1=document.getElementById("number1");
     let number2=document.getElementById("number2");
-    let result=Number(number1.value)+Number(number2.value);
-    document.getElementById("result").value=result;
+    let n= number1.value;
+    let m= number2.value;
+    const response = await fetch("http://localhost:3000/add/"+n+"/"+m);
+    const sum = await response.json();
+    
+    document.getElementById('result').value = sum;
 };
