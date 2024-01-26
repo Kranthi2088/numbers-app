19acf85ef4a8e6a79ede7a57ad5ba81b3660ff77
Dunna, Kranthi Kumar <kdunna@iu.edu>
2024-01-25 19:28:28 -0500

    commited

---
+++ arithmetic-service/base.js
@@ -0,0 +1,9 @@
+async function add(){
+    let number1=document.getElementById("number1");
+    let number2=document.getElementById("number2");
+    let n= number1.value;
+    let m= number2.value;
+    const response = await fetch("http://localhost:3000/add/"+n+"/"+m);
+    const sum= await response.json();
+    document.getElementById('sum').innerText=sum;
+};
