52034aabf5cb64895d5b7121d92c73e3d770355d
Dunna, Kranthi Kumar <kdunna@iu.edu>
2024-02-09 01:47:58 -0500

    practicum_4_changes

--- base.js
+++ base.js
@@ -3,7 +3,7 @@ async function add(){
     let number2=document.getElementById("number2");
     let n= number1.value;
     let m= number2.value;
-    const response = await fetch("http://localhost:3000/add/"+n+"/"+m);
+    const response = await fetch("https://arithmetic-service-kranthi2088.onrender.com/add/"+n+"/"+m);
     const sum = await response.json();
     
     document.getElementById('result').value = sum;
