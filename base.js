6d96f3d660cb47f08e6e090ebd29fa120b3c196f
ambdasa <ambicadasari.11@gmail.com>
2024-02-22 18:52:44 -0500

    recent

--- base.js
+++ base.js
@@ -3,8 +3,16 @@ async function add(){
     let number2=document.getElementById("number2");
     let n= number1.value;
     let m= number2.value;
-    const response = await fetch("http://localhost:3000/add/"+n+"/"+m);
+    const response = await fetch("https://arithmetic-service-kranthi2088.onrender.com/add/"+n+"/"+m);
     const sum = await response.json();
     
     document.getElementById('result').value = sum;
 };
+
+async function isprime(){
+    const numberToTest = document.getElementById("primenumber");            
+    let n = numberToTest.value;            
+    const response = await fetch("???" + n);
+    const result = await response.json();
+    document.getElementById("primeCheckResult").innerText = result;
+}
